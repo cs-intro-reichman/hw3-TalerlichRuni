7ccda6d234335bea23ab7fecfb63a73ab2f4dc69
tal erlich <tal.erlich@post.runi.ac.il>
2024-11-20 23:26:04 +0200

    C

--- Anagram.java
+++ Anagram.java
@@ -1,19 +1,11 @@
-/** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent", "listen"));  // true
-		System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie", "Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
-
-		// Tests the preProcess function.
+		System.out.println(isAnagram("silent", "listen"));
+		System.out.println(isAnagram("William Shakespeare", "I am a weakish speller"));
+		System.out.println(isAnagram("Madam Curie", "Radium came"));
+		System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort"));
 		System.out.println(preProcess("What? No way!!!"));
-
-		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-
-		// Performs a stress test of randomAnagram 
 		String str = "1234567";
 		boolean pass = true;
 		for (int i = 0; i < 10; i++) {
@@ -25,7 +17,6 @@ public class Anagram {
 		System.out.println(pass ? "test passed" : "test failed");
 	}
 
-	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 		boolean result = true;
 		str1 = preProcess(str1);
@@ -54,8 +45,6 @@ public class Anagram {
 		return result;
 	}
 
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, spaces are preserved, and all the other characters are deleted.
 	public static String preProcess(String str) {
 		String processed = "";
 		for (int i = 0; i < str.length(); i++) {
@@ -69,8 +58,6 @@ public class Anagram {
 		return processed;
 	}
 
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. Spaces are preserved.
 	public static String randomAnagram(String str) {
 		str = preProcess(str);
 		String result = "";
